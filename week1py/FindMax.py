1a5e3e4529313b937b60c2ef4ec17009c4ef6dfe
Troels Just Christoffersen <troelsjc@gmail.com>
2017-02-10 23:43:30 +0100

    init

---
+++ week1py/FindMax.py
@@ -0,0 +1,11 @@
+def findMax():
+    # A = [1, 3, 7, 15, 17, 11, 2, 3, 6, 8, 7, 5, 9, 5, 23]
+    n = input()
+    A = list(map(int, input().split()))
+    max = 0
+    for i in range(len(A)):
+        if A[i] > A[max]:
+            max = i
+    print(max)
+    return max
+findMax()
