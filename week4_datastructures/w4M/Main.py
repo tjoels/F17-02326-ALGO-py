b02fd64e3e89910000c0de07a65170b3c0749ec8
Troels Just Christoffersen <troelsjc@gmail.com>
2017-03-05 19:01:21 +0100

    this resolves M.2

---
+++ week4_datastructures/w4M/Main.py
@@ -0,0 +1,47 @@
+"""
+Implemented with the aid of
+CLRS Chapter 2 insertion sort pseudo code.
+A is an array that is to get sorted.
+"""
+def insertionsort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        # position = A[j - 1]
+        # Insert  A[j] into sorted sequence A[1 .. j - 1].
+        i = j - 1
+        while i >= 0 and A[i] > key:
+            A[i + 1] = A[i]
+            i = i - 1
+        A[i + 1] = key
+    return A
+
+'''
+Generates arrays of increasing sizes populated with random integers
+'''
+from random import sample
+
+def randomintarray(listAmount):
+    aList = [] # create aList object of type list
+    for size in range(1, listAmount + 1):
+        list = sample(range(1000), k=size) # create list of increasing size with random integers
+        #print(list)
+        aList.append(list) # append list to aList
+    print(aList)
+    return aList
+
+'''
+Calls randomintarray to generate lists,
+and sorts them by calling insertionsort upon each list
+returns list of sorted lists
+'''
+def randomsorter(listAmount):
+    currentList = randomintarray(listAmount)
+    sortedList = []
+    for i in range(0, listAmount):
+        #print('I' + currentList[i])
+        sortedList.append(insertionsort(currentList[i]))
+        #print('O' + sortedList)
+    print(sortedList)
+    return sortedList
+
+randomsorter(10)
