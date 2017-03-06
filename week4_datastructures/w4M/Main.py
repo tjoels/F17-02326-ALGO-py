e566b0c19b704715190ec91271547a56cea708a4
trules <troelsjc@gmail.com>
2017-03-06 16:46:00 +0100

    sample range is now size

--- week4_datastructures/w4M/Main.py
+++ week4_datastructures/w4M/Main.py
@@ -23,10 +23,10 @@ from random import sample
 def randomintarray(listAmount):
     aList = [] # create aList object of type list
     for size in range(1, listAmount + 1):
-        list = sample(range(1000), k=size) # create list of increasing size with random integers
+        list = sample(range(size), k=size) # create list of increasing size with random integers in range(size)
         #print(list)
         aList.append(list) # append list to aList
-    print(aList)
+    #print(aList)
     return aList
 
 '''
