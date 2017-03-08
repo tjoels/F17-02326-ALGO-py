b5c39b00be858e1f9a9827b38c1433337c8f0b0b
Troels Just Christoffersen <troelsjc@gmail.com>
2017-03-09 00:20:35 +0100

    timings

--- week4_datastructures/w4M/Main.py
+++ week4_datastructures/w4M/Main.py
@@ -20,11 +20,11 @@ Generates arrays of increasing sizes populated with random integers
 '''
 from random import sample
 
-def randomintarray(listAmount):
+def randomintarray(startingnumber, listAmount):
     aList = [] # create aList object of type list
-    for size in range(1, listAmount + 1):
+    for size in range(startingnumber, listAmount + 1, 5000):
         list = sample(range(size), k=size) # create list of increasing size with random integers in range(size)
-        #print(list)
+        print(list)
         aList.append(list) # append list to aList
     #print(aList)
     return aList
@@ -34,17 +34,19 @@ Calls randomintarray to generate lists,
 and sorts them by calling insertionsort upon each list
 returns list of sorted lists
 '''
-#import time
+import time
 #millis = int(round(time.time() * 1000))
 #print millis
-def randomsorter(listAmount):
-    currentList = randomintarray(listAmount)
+def randomsorter(startingnumber, listAmount):
+    currentList = randomintarray(startingnumber, listAmount)
     sortedList = []
-    for i in range(0, listAmount):
-        #print('I' + currentList[i])
+    for i in range(0, listAmount-startingnumber+1): # Loops through generated lists (diff of interval plus 1)
+        startmillis = int(round(time.time() * 1000))
         sortedList.append(insertionsort(currentList[i]))
-        #print('O' + sortedList)
+        endmillis = int(round(time.time() * 1000))
+        timetaken = endmillis-startmillis
+        print("Sortingtime", (i*5000)+startingnumber, "elements:", timetaken)
     print(sortedList)
     return sortedList
 
-randomsorter(10)
+randomsorter(0, 20000)
